dfbe1193348b4ce93d192cdb2b7dc3cdeed2f5ca
PrimeSandy <gowtham2006x@gmail.com>
2025-02-20 19:50:46 +0530

    First File

--- Music-Player-main/script.js
+++ Music-Player-main/script.js
@@ -22,28 +22,28 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img : 'images/Raayan.png',
+        name : 'Raayan',
+        artist : 'Anirudh Ravichander',
+        music : 'music/Raayan.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        img : 'images/Moonu.png',
+        name : '3 Moonu bgm',
+        artist : 'Anirudh Ravichander',
+        music : 'music/Moonu.mp3'
     },
     {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+        img : 'images/illuminati.png',
+        name : 'Illuminati',
+        artist : 'A.R.Rahman',
+        music : 'music/Illuminati.mp3'
     },
     {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        img : 'images/Golden.png',
+        name : 'Golden Sparrow',
+        artist : 'G.v Prakash',
+        music : 'music/Golden Sparrow.mp3'
     }
 ];
 
